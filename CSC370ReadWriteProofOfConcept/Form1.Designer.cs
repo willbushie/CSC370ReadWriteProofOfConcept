f96bbf2eb0328ddfa8c1da9b52e78771044b1f66
William Bushie <willbushie@gmail.com>
2021-03-24 19:07:28 -0500

    Completion of the read functionality. Began working on the "write" & "save as" functions. The current working application is still very hardcoded.

--- CSC370ReadWriteProofOfConcept/Form1.Designer.cs
+++ CSC370ReadWriteProofOfConcept/Form1.Designer.cs
@@ -29,20 +29,155 @@ namespace CSC370ReadWriteProofOfConcept
         /// </summary>
         private void InitializeComponent()
         {
+            this.textFileLabel = new System.Windows.Forms.Label();
+            this.textFileTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.contentsLabel = new System.Windows.Forms.Label();
+            this.contentsTextBox = new System.Windows.Forms.TextBox();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.writeButton = new System.Windows.Forms.Button();
+            this.saveAsButton = new System.Windows.Forms.Button();
+            this.contentsTextBox2 = new System.Windows.Forms.TextBox();
+            this.contentsLabel2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textFileLabel
+            // 
+            this.textFileLabel.AutoSize = true;
+            this.textFileLabel.Location = new System.Drawing.Point(12, 16);
+            this.textFileLabel.Name = "textFileLabel";
+            this.textFileLabel.Size = new System.Drawing.Size(47, 13);
+            this.textFileLabel.TabIndex = 0;
+            this.textFileLabel.Text = "Text File";
+            // 
+            // textFileTextBox
+            // 
+            this.textFileTextBox.Location = new System.Drawing.Point(75, 12);
+            this.textFileTextBox.Name = "textFileTextBox";
+            this.textFileTextBox.Size = new System.Drawing.Size(217, 20);
+            this.textFileTextBox.TabIndex = 1;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(75, 63);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(81, 60);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // contentsLabel
+            // 
+            this.contentsLabel.AutoSize = true;
+            this.contentsLabel.Location = new System.Drawing.Point(7, 157);
+            this.contentsLabel.Name = "contentsLabel";
+            this.contentsLabel.Size = new System.Drawing.Size(52, 13);
+            this.contentsLabel.TabIndex = 3;
+            this.contentsLabel.Text = "Contents:";
+            // 
+            // contentsTextBox
+            // 
+            this.contentsTextBox.Location = new System.Drawing.Point(75, 157);
+            this.contentsTextBox.Multiline = true;
+            this.contentsTextBox.Name = "contentsTextBox";
+            this.contentsTextBox.Size = new System.Drawing.Size(217, 204);
+            this.contentsTextBox.TabIndex = 4;
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(211, 63);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(81, 60);
+            this.openFileButton.TabIndex = 5;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(138, 378);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear All";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // writeButton
+            // 
+            this.writeButton.Location = new System.Drawing.Point(410, 63);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(81, 60);
+            this.writeButton.TabIndex = 7;
+            this.writeButton.Text = "Write";
+            this.writeButton.UseVisualStyleBackColor = true;
+            this.writeButton.Click += new System.EventHandler(this.writeButton_Click);
+            // 
+            // saveAsButton
+            // 
+            this.saveAsButton.Location = new System.Drawing.Point(538, 63);
+            this.saveAsButton.Name = "saveAsButton";
+            this.saveAsButton.Size = new System.Drawing.Size(81, 60);
+            this.saveAsButton.TabIndex = 8;
+            this.saveAsButton.Text = "Save As";
+            this.saveAsButton.UseVisualStyleBackColor = true;
+            this.saveAsButton.Click += new System.EventHandler(this.saveAsButton_Click);
+            // 
+            // contentsTextBox2
+            // 
+            this.contentsTextBox2.Location = new System.Drawing.Point(410, 157);
+            this.contentsTextBox2.Multiline = true;
+            this.contentsTextBox2.Name = "contentsTextBox2";
+            this.contentsTextBox2.Size = new System.Drawing.Size(209, 204);
+            this.contentsTextBox2.TabIndex = 9;
+            // 
+            // contentsLabel2
+            // 
+            this.contentsLabel2.AutoSize = true;
+            this.contentsLabel2.Location = new System.Drawing.Point(343, 160);
+            this.contentsLabel2.Name = "contentsLabel2";
+            this.contentsLabel2.Size = new System.Drawing.Size(52, 13);
+            this.contentsLabel2.TabIndex = 10;
+            this.contentsLabel2.Text = "Contents:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(695, 450);
+            this.Controls.Add(this.contentsLabel2);
+            this.Controls.Add(this.contentsTextBox2);
+            this.Controls.Add(this.saveAsButton);
+            this.Controls.Add(this.writeButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.contentsTextBox);
+            this.Controls.Add(this.contentsLabel);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.textFileTextBox);
+            this.Controls.Add(this.textFileLabel);
             this.Name = "Form1";
             this.Text = "Read and Write to Text File";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label textFileLabel;
+        private System.Windows.Forms.TextBox textFileTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label contentsLabel;
+        private System.Windows.Forms.TextBox contentsTextBox;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button writeButton;
+        private System.Windows.Forms.Button saveAsButton;
+        private System.Windows.Forms.TextBox contentsTextBox2;
+        private System.Windows.Forms.Label contentsLabel2;
     }
 }
 
--- CSC370ReadWriteProofOfConcept/Form1.cs
+++ CSC370ReadWriteProofOfConcept/Form1.cs
@@ -33,40 +33,116 @@ namespace CSC370ReadWriteProofOfConcept
             InitializeComponent();
         }
 
-        /*
-         * This method obtains the necessary infromation from the file to be
-         * used later in the application
-         */
-        public void obtainFileInfo()
+        // this button event clear the text file & contents text boxes
+        private void clearButton_Click(object sender, EventArgs e)
         {
-
+            this.contentsTextBox.Text = "";
+            this.textFileTextBox.Text = "";
         }
 
-        /* 
-         * This method is given a file, chosen by the user and outputs the 
-         * contents of that file to a textbox in the form.
-         */
-        public void readFromFile()
-        {
 
+        // this is the button event of showButton
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // obtain the file name from the user
+            string filename = this.textFileTextBox.Text;
+
+            // read the entire fiel as one string
+            string text = System.IO.File.ReadAllText(path: @filename); 
+
+            // display the file contents to the output window
+            this.contentsTextBox.Text = text;
         }
 
-        /* 
-         * This method writes to a file at a specified location from the user.
-         * This method takes a specified file given by the user &
-         * writes to the given file the information input. 
-         */
-        public void writeToFile()
+        // this is the button event of opening a file location
+        // it will read the file and enter its contents into the contents text box,
+        // as well as input the file path in the text file text box
+        private void openFileButton_Click(object sender, EventArgs e)
         {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // get the path of the spcified file 
+                    filePath = openFileDialog.FileName;
+
+                    // read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            // place the file path into the text file text box
+            this.textFileTextBox.Text = filePath;
+
+            // output the contents of the stream to the contents text box
+            this.contentsTextBox.Text = fileContent;
+        }
+
+
+        // this is the button event for writing to a file
+        // as of now, everything is hardcoded - the name of the file & the contents
+        // this method successfully creates a new file with the hardcoded information in the documents folder
+        private void writeButton_Click(object sender, EventArgs e)
+        {
+            // create a string array with the lines of text
+            string[] lines = {"first line", "second line", "third line"};
+
+            // set a variable to the documents path
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // write the string array to a new file named "WriteLines.txt"
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+            {
+                foreach (string line in lines)
+                {
+                    outputFile.WriteLine(line);
+                }
+            }
+
             // all of this code is from a previous application, leaving it in this format to 
             // help in the future when implementing the writeToFile method
-            string fileName = DateTime.Now.ToString("MMddyyyy_HHmmss") + "_ShimTotals.txt";
+            /*string fileName = DateTime.Now.ToString("MMddyyyy_HHmmss") + "_ShimTotals.txt";
             string documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             using (StreamWriter outputFile = new StreamWriter(Path.Combine(documentPath, fileName)))
             {
                 // write the header for the file
                 outputFile.WriteLine(DateTime.Now.ToString("MM/dd/yyyy | HH:mm:ss"));
                 outputFile.WriteLine("TotalShims: " + " | Total Weight: ");
+            }
+            */
+        }
+
+        // this is the button event for saving as
+        // 
+        private void saveAsButton_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // code to write the stream goes here
+                    myStream.Close();
+                    MessageBox.Show("this is a test");
+                }
             }
         }
     }
